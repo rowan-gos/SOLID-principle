dab857d1514dd6a934b093bf1cb26132aface605
rowan-gos <rowan.phan.gos@gmail.com>
2026-04-02 12:16:57 +0700

    add initial implementation of printers and scanning functionality; create main and sample files

---
+++ Solid/4.Isp/main.ts
@@ -0,0 +1,19 @@
+/**
+ * *****************************************
+ * 📝 UNCOMMENT THE CODE BELOW AND BEGIN YOUR SOLUTION:
+ * *****************************************
+ *
+ * The following lines are currently commented out.
+ * Uncomment them to start implementing your solution.
+ * Happy coding! 🚀
+ */
+
+import { OldFashionedPrinter, MultiFunctionPrinter } from "./sample";
+
+const oldPrinter = new OldFashionedPrinter();
+oldPrinter.print("Document 1");
+
+const multiFunctionPrinter = new MultiFunctionPrinter();
+multiFunctionPrinter.print("Document 2");
+multiFunctionPrinter.scan("Document 3");
+multiFunctionPrinter.fax("Document 4");
---
+++ Solid/4.Isp/sample.ts
@@ -0,0 +1,5 @@
+export { IPrinter } from "./iPrinter";
+export { IScanner } from "./iScanner";
+export { IFax } from "./iFax";
+export { OldFashionedPrinter } from "./oldFashionedPrinter";
+export { MultiFunctionPrinter } from "./multiFunctionPrinter";
